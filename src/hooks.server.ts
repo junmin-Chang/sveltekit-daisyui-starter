06650cf000ffa5febe962df59170ee6ec0316342
z00mni <chjm219@kookmin.ac.kr>
2023-03-09 01:05:19 +0900

    add google login

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,12 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
-import Kakao from "@auth/core/providers/kakao";
+
 import {
   GITHUB_ID,
   GITHUB_SECRET,
   GOOGLE_ID,
   GOOGLE_SECRET,
-  KAKAO_ID,
-  KAKAO_SECRET,
   SECRET,
 } from "$env/static/private";
 
@@ -20,11 +18,6 @@ export const handle = SvelteKitAuth({
     Google({
       clientId: GOOGLE_ID,
       clientSecret: GOOGLE_SECRET,
-    }),
-    // @ts-expect-error issue https://github.com/nextauthjs/next-auth/issues/6174
-    Kakao({
-      clientId: KAKAO_ID,
-      clientSecret: KAKAO_SECRET,
     }),
   ],
   secret: SECRET,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script>
     import { page } from '$app/stores';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        console.log($page.data)
+    })
 </script>
 <h2>PAGE CONTENT</h2>
 
 <p>
 
     {$page.data.session?.user?.email}
+
+
 
 </p>
